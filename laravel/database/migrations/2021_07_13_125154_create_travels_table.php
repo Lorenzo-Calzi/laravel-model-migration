cf5a9b827ebba1dc78f63fc81606e00f0b49eeb6
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-13 15:13:15 +0200

    Aggiunte entità

---
+++ laravel/database/migrations/2021_07_13_125154_create_travels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 100);
+            $table->string('departure_date', 20);
+            $table->string('return_date', 20);
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
